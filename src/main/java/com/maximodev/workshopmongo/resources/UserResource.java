2b7d1ca16c636e5dc75a78e9be10cbf39b6b72a6
Felipe Maximo <felipecontato2001@hotmail.com>
2023-12-06 16:35:37 -0300

    user delete

--- src/main/java/com/maximodev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/maximodev/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,11 +40,17 @@ public class UserResource {
     }
 
     @PostMapping
-    public ResponseEntity<Void> findById(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
         User user = userService.fromDTO(userDTO);
         user = userService.insert(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
